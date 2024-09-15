0f7dd4a5c99489fa74855b46e30486d76c98e9c2
tumee <tmkee0525@gmail.com>
2024-09-15 13:59:11 +0900

    change to sns setting

--- mtche-news/resources/views/article.blade.php
+++ mtche-news/resources/views/article.blade.php
@@ -102,10 +102,10 @@
             @endif
             <img class="img-fluid mt-3 mb-5 rounded" loading="lazy" src="{{ asset('storage/' . $article->image) }}" alt="">
             <div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v10.0" nonce="dQotufLO"></script>
-<div class="fb-share-button" data-href="YOUR_PAGE_URL" data-layout="button" data-size="large">
-  <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=YOUR_PAGE_URL&src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a>
-</div>
+            <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v10.0" nonce="dQotufLO"></script>
+            <div class="fb-share-button" data-href={{route('higthArticle',['id' => $article->id])}} data-layout="button" data-size="large">
+             <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=.{{route('higthArticle',['id' => $article->id])}}.&src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a>
+            </div>
           </div>
         </div>
       </div>
