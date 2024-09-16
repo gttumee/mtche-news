4ddbfc99457c46b99426b8ddafedd2a2911dce51
tumee <tmkee0525@gmail.com>
2024-09-16 18:56:08 +0900

    change to facebook title

--- mtche-news/resources/views/layouts/common.blade.php
+++ mtche-news/resources/views/layouts/common.blade.php
@@ -12,7 +12,7 @@
   <meta name="author" content="BootstrapBrain">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta property="og:title" content="@yield('og_title', 'MTCHE Media')">
-  <meta property="og:description" content="@yield('og_description', '最も興味深い情報を皆様と共有させていただきます。Та бүхэнд хамгийн сонирхолтой мэдээлэлийг түгээх болно.')">
+  <meta property="og:description" content="@yield('og_description', 'Инноваци сэтгэлгээг үргэлж дэмжинэ. Бүтээмж бол амжилтын үндэс.')">
   <meta property="og:image" content="@yield('og_image', 'https://media.mtche.jp/assets/img/about/title.jpg')">
   <meta property="og:url" content="@yield('og_url', url()->current())">
   <meta property="og:type" content="article">
