694e5175c4015973aa9d38d2929d14999d3d5c8f
tumee <tmkee0525@gmail.com>
2024-12-08 10:38:15 +0900

    user setting

--- mtche-news/app/Models/User.php
+++ mtche-news/app/Models/User.php
@@ -3,14 +3,22 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@mtche.jp') && $this->hasVerifiedEmail();
+    }
 
     /**
      * The attributes that are mass assignable.
